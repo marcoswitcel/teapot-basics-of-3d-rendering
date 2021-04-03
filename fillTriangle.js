258121d121690b5bbbd936a32d24dd267fd80359
João Marcos <joao@agenciagh.com.br>
2021-04-03 17:44:53 -0300

    galore

---
+++ fillTriangle.js
@@ -0,0 +1,87 @@
+function cross(a, b, c) {
+    return (b.x - a.x) * -(c.y - a.y) - -(b.y - a.y) * (c.x - a.x);
+}
+
+/**
+ * 
+ * @param {ImageData} imageData 
+ * @param {Object} v0 
+ * @param {Object} v1 
+ * @param {Object} v2 
+ */
+export default function fillTriangle(imageData, v0, v1, v2)
+{
+    var minX = Math.floor(Math.min(v0.x, v1.x, v2.x));
+    var maxX = Math.ceil(Math.max(v0.x, v1.x, v2.x));
+    var minY = Math.floor(Math.min(v0.y, v1.y, v2.y));
+    var maxY = Math.ceil(Math.max(v0.y, v1.y, v2.y));
+  
+    var data = imageData.data;
+    var width = imageData.width;
+  
+    // precalculate the area of the parallelogram defined by our triangle
+    var area = cross(v0, v1, v2);
+  
+    // get all properties on our first vertex, for interpolating later
+    var props = Object.getOwnPropertyNames(v0);
+    
+    // p is our 2D pixel location point
+    var p = {};  
+    
+    // fragment is the resulting pixel with all the vertex attributes interpolated
+    var fragment = {};
+    
+    for (var y = minY; y < maxY; y++) {
+        for (var x = minX; x < maxX; x++) {
+            // sample from the center of the pixel, not the top-left corner
+            p.x = x + 0.5; p.y = y + 0.5;
+
+            // calculate vertex weights
+            // should divide these by area, but we do that later
+            // so we divide once, not three times
+            var w0 = cross(v1, v2, p);
+            var w1 = cross(v2, v0, p);
+            var w2 = cross(v0, v1, p);
+  
+            // calculate edges
+            var edge0 = { x: v2.x - v1.x, y: v2.y - v1.y };
+            var edge1 = { x: v0.x - v2.x, y: v0.y - v2.y };
+            var edge2 = { x: v1.x - v0.x, y: v1.y - v0.y };
+            
+            // calculate which edges are right edges so we can easily skip them
+            // right edges go up, or (bottom edges) are horizontal edges that go right
+            var edgeRight0 = edge0.y < 0 || (edge0.y == 0 && edge0.x > 0);
+            var edgeRight1 = edge1.y < 0 || (edge1.y == 0 && edge0.x > 0);
+            var edgeRight2 = edge2.y < 0 || (edge2.y == 0 && edge0.x > 0);
+
+            // if the point is not inside our polygon, skip fragment
+            if (w0 < 0 || w1 < 0 || w2 < 0) {
+                continue;
+            }
+
+                  
+            // if this is a right or bottom edge, skip fragment (top-left rule):
+            if ((w0 == 0 && edgeRight0) || (w1 == 0 && edgeRight1) || (w2 == 0 && edgeRight2)) {
+                continue;
+            }
+
+            // interpolate our vertices
+            for (var i = 0; i < props.length; i++) {
+                var prop = props[i];
+
+                // divide by area here to normalize
+                fragment[prop] = (w0 * v0[prop] + w1 * v1[prop] + w2 * v2[prop]) / area;
+            }
+
+            // returns true and replaces the value if fragment.z is less than the stored value
+            if (typeof fragment.z !== "number" || depthBuffer.testDepth(x, y, fragment.z)) {
+                // set pixel
+                var index = (y * width + x) * 4;
+                data[index] = typeof fragment.r === "number" ? (fragment.r * 256) | 0 : 0;
+                data[index + 1] = typeof fragment.g === "number" ? (fragment.g * 256) | 0 : 0;
+                data[index + 2] = typeof fragment.b === "number" ? (fragment.b * 256) | 0 : 0;
+                data[index + 3] = typeof fragment.a === "number" ? (fragment.a * 256) | 0 : 255;
+            }
+        }
+    }
+}
--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 //@ts-check
 import drawLine from './drawline.js';
 import parseObj from './parseObj.js';
+import fillTriangle from './fillTriangle.js';
 
 // add all properties on the IO object to the window object
 // so they can be used without prefixing "IO."
 usingModule(IO);
 var model = null;
+var canvas;
+
 
 // main function called on load
 function run() {
-  // loads list of files asynchronously, calls function when done
-  File.loadFiles(
-    function(files) {
-      // parse our teapot obj model file
-      model = parseObj(files[0].value);
-      // start main program
-      start();
-    },
-    "teapot.obj"
-  );
+    // loads list of files asynchronously, calls function when done
+    File.loadFiles(
+        function(files) {
+            // parse our teapot obj model file
+            model = parseObj(files[0].value);
+            // start main program
+            start();
+        },
+        "teapot.obj"
+    );
 }
 
 function crossProduct(a, b) {
     return {
-      x: a.y * b.z - a.z * b.y,
-      y: a.z * b.x - a.x * b.z,
-      z: a.x * b.y - a.y * b.x
+        x: a.y * b.z - a.z * b.y,
+        y: a.z * b.x - a.x * b.z,
+        z: a.x * b.y - a.y * b.x
     };
+}
+
+function clear(context, color) {
+    context.fillStyle = color;
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 // returns true if vertices are in counterclockwise order
@@ -35,48 +43,112 @@ function isCcw(v0, v1, v2) {
 }
 
 function start() {
-  // set our canvas size
-  var canvas = document.getElementById("canvas");
-  canvas.width = 800;
-  canvas.height = 480;
-  
-  // clear our canvas to opaque black
-  var context = canvas.getContext("2d");
-  context.fillStyle = "black";
-  context.fillRect(0, 0, canvas.width, canvas.height);
-  
-  // get image data for direct pixel access
-  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    // set our canvas size
+    canvas = document.getElementById("canvas");
+    canvas.width = 800;
+    canvas.height = 480;
+    
+    // create our zFar and zNear clip planes
+    var zFar = -2.5;
+    var zNear = 2.5; 
+    
+    // create depth buffer
+    // use Uint16 because we only need a little precision and we save 2 bytes per pixel this way
+    window.depthBuffer = new Uint16Array(canvas.width * canvas.height);
+
+    // clear our canvas to opaque black
+    var context = canvas.getContext("2d");
+    // context.fillStyle = "black";
+    // context.fillRect(0, 0, canvas.width, canvas.height);
+    clear(context, "rgb(100, 149, 237)");
+
+    // get image data for direct pixel access
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+      // add some properties and methods to make using this easier
+    depthBuffer.width = canvas.width;
+    depthBuffer.height = canvas.height;
+    depthBuffer.clear = function() { this.fill(65535); };
+    depthBuffer.getDepth = function(x, y) { return this[y * this.width + x] / 65535.0; };
+    depthBuffer.setDepth = function(x, y, v) { this[y * this.width + x] = (v * 65535) | 0; };
+    depthBuffer.testDepth = function(x, y, v) {
+        var value = (v * 65535) | 0;
+        if (value < 0 || value > 65535) {
+        return false;
+        }
+        var index = y * this.width + x;
+        if (value < this[index]) {
+        this[index] = value;
+        return true;
+        }
+        return false;
+    };
+    
+    // clear depth buffer and attach to imageData
+    depthBuffer.clear();
+    imageData.depthBuffer = depthBuffer;
 
     // some drawing positioning
     var centerX = canvas.width / 2.0;
     var centerY = canvas.height / 2.0;  
     var scale = 100;
     centerY += 150;
-    
+
     console.time('tempo para rasterizar as linhas');
     // draw our model
     for (var i = 0; i < model.faces.length; i++) {
-      var face = model.faces[i];
-      var v0 = model.verts[face[0] - 1];
-      var v1 = model.verts[face[1] - 1];
-      var v2 = model.verts[face[2] - 1];
-      
-      if (v0 && v1 && v2) {
-        if (isCcw(v0, v1, v2)) {
-            drawLine(imageData, centerX + v0.x * scale, centerY - v0.y * scale, centerX + v1.x * scale, centerY - v1.y * scale);
-            drawLine(imageData, centerX + v1.x * scale, centerY - v1.y * scale, centerX + v2.x * scale, centerY - v2.y * scale);
-            drawLine(imageData, centerX + v2.x * scale, centerY - v2.y * scale, centerX + v0.x * scale, centerY - v0.y * scale);
+        var face = model.faces[i];
+        var v0 = model.verts[face[0] - 1];
+        var v1 = model.verts[face[1] - 1];
+        var v2 = model.verts[face[2] - 1];
+        
+        if (v0 && v1 && v2) {
+            if (isCcw(v0, v1, v2)) {
+                // create some greyscale values from the model's Z values
+                var v0value = v0.z / 4.5 + 0.5;
+                var v1value = v1.z / 4.5 + 0.5;
+                var v2value = v2.z / 4.5 + 0.5;
+                    
+                // drawLine(imageData, centerX + v0.x * scale, centerY - v0.y * scale, centerX + v1.x * scale, centerY - v1.y * scale);
+                // drawLine(imageData, centerX + v1.x * scale, centerY - v1.y * scale, centerX + v2.x * scale, centerY - v2.y * scale);
+                // drawLine(imageData, centerX + v2.x * scale, centerY - v2.y * scale, centerX + v0.x * scale, centerY - v0.y * scale);
+                fillTriangle(
+                    imageData,
+                    { x: centerX + v0.x * scale, y: centerY - v0.y * scale, z: (v0.z - zNear) / (zFar - zNear),
+                      r: v0value, g: v0value, b: v0value },
+                    { x: centerX + v1.x * scale, y: centerY - v1.y * scale, z: (v1.z - zNear) / (zFar - zNear),
+                      r: v1value, g: v1value, b: v1value },
+                    { x: centerX + v2.x * scale, y: centerY - v2.y * scale, z: (v2.z - zNear) / (zFar - zNear),
+                      r: v2value, g: v2value, b: v2value }
+                );
+            }
         }
-      }
-      else {
-        if (!v0) { console.log("Vertice " + (face[0] - 1) + " not found!"); }
-        if (!v1) { console.log("Vertice " + (face[1] - 1) + " not found!"); }
-        if (!v2) { console.log("Vertice " + (face[2] - 1) + " not found!"); }
-      }
+        else {
+            if (!v0) { console.log("Vertice " + (face[0] - 1) + " not found!"); }
+            if (!v1) { console.log("Vertice " + (face[1] - 1) + " not found!"); }
+            if (!v2) { console.log("Vertice " + (face[2] - 1) + " not found!"); }
+        }
     }
+    // fillTriangle(
+    //     imageData,
+    //     { x: 200.5, y: 400.5, z: 0, r: 1.0, g: 0.0, b: 0.0 },
+    //     { x: 250.5, y: 150.5, z: 0, r: 0.0, g: 1.0, b: 0.0 },
+    //     { x: 90.5, y: 250.5, z: 0, r: 0.0, g: 0.0, b: 1.0 }
+    // );
+    // fillTriangle(
+    //     imageData,
+    //     { x: 10, y: 10, z: 0, r: 0.3, g: 0.3, b: 0.3 },
+    //     { x: 10, y: 110, z: 0, r: 0.3, g: 0.3, b: 0.3 },
+    //     { x: 110, y: 10, z: 0, r: 0.3, g: 0.3, b: 0.3 }
+    // );
+    // fillTriangle(
+    //     imageData,
+    //     { x: 110, y: 10, z: 0, r: 0.3, g: 0.3, b: 0.3 },
+    //     { x: 10, y: 110, z: 0, r: 0.3, g: 0.3, b: 0.3 },
+    //     { x: 110, y: 110, z: 0, r: 0.3, g: 0.3, b: 0.3 }
+    // );
     console.timeEnd('tempo para rasterizar as linhas');
-    
+
     // write our new pixels to the canvas
     context.putImageData(imageData, 0, 0);
 }
